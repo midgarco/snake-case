9fd084b59db2aea34d0debc64d5b1cb71b0c5a40
Jeff Dupont <jdupont@brightcove.com>
2017-07-28 11:54:03 -0700

    initial commit

---
+++ spec/snake-case-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SnakeCaseView from '../lib/snake-case-view';
+
+describe('SnakeCaseView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
